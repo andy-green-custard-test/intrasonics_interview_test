8be45c4bb1058e64ac7047770966c6119eebb920
andy33gmail <andy.thomas@taptilegames.com>
2020-04-10 00:29:02 +0100

    Removed thread pre-computing to remove glitches

--- app/src/main/java/com/intrasonics/mobile/numericanalyser/feature/juliaset/JuliaGraphView.kt
+++ app/src/main/java/com/intrasonics/mobile/numericanalyser/feature/juliaset/JuliaGraphView.kt
@@ -1,17 +1,12 @@
 package com.intrasonics.mobile.numericanalyser.feature.juliaset
 
 import android.content.Context
-import android.graphics.Bitmap
 import android.graphics.Canvas
 import android.graphics.Color
 import android.graphics.Paint
 import android.graphics.Paint.ANTI_ALIAS_FLAG
-import android.os.Handler
-import android.os.Looper
 import android.util.AttributeSet
 import android.view.View
-import java.util.concurrent.Executors
-import java.util.concurrent.Future
 
 class JuliaGraphView : View {
     constructor(context: Context) : this(context, null)
@@ -25,32 +20,43 @@ class JuliaGraphView : View {
         color = Color.BLACK
     }
 
-    val precomputeExector = Executors.newSingleThreadExecutor()
-    private var presentFuture: Future<*>? = null // Future for any current rendering operation
-    var bitmap: Bitmap? = null // Canvas for pre-rendering
-
-    val mainThreadHandler = Handler(Looper.getMainLooper())
+    private var graphRules: GraphRules? = null
+    private var stepSizePx: Int = 0
 
     lateinit var onDrawCallback: () -> Unit?
     lateinit var onResizeCallback: () -> Unit?
 
     override fun onDraw(canvas: Canvas?) {
-        if (presentFuture != null)
-            return // abort, since bitmap is being mutated  - note: this.invalidate() is guaranteed to be called when it finishes
+        graphRules?.let { graphRules ->
+            canvas?.apply {
+                canvas.drawRect(0f, 0f, width.toFloat(), height.toFloat(), divergentPaint) // Clear canvas
 
-        canvas?.apply {
-            canvas.drawBitmap(bitmap, 0f, 0f, null)
-            onDrawCallback()
+                // Ideally, batches would be sent to threads (e.g. have a thread per row, or per 100 points)
+                for (x in 0..width step stepSizePx) {
+                    val r = graphRules.minReal + (graphRules.maxReal - graphRules.minReal) * x / width
+                    for (y in 0..height step stepSizePx) {
+                        val i = graphRules.minImaginary + (graphRules.maxImaginary - graphRules.minImaginary) * y / height
+                        if (isIn(r, i, graphRules)) {
+                            canvas.drawRect(x.toFloat(), // Chunk into squares of size stepSize to reduce the amount of computation required
+                                    y.toFloat(),
+                                    (x + stepSizePx).toFloat(),
+                                    (y + stepSizePx).toFloat(),
+                                    convergentPaint)
+                        }
+                    }
+                }
+
+                onDrawCallback()
+            }
         }
     }
 
     override fun onSizeChanged(w: Int, h: Int, oldw: Int, oldh: Int) {
-        bitmap = Bitmap.createBitmap(w, h, Bitmap.Config.ARGB_4444) // Ideally would define this a 1 bit/pixel
-        // TODO: fix obvious thread safety problem
         onResizeCallback()
         super.onSizeChanged(w, h, oldw, oldh)
     }
 
+    // This really really ought to be in its own provider, with unit tests.
     fun isIn(r: Float, i: Float, graphRules: GraphRules): Boolean {
         val limit = graphRules.veryBig * graphRules.veryBig // Avoid square root at all costs (powers more expensive than multiplying). Also avoid repeated operations
 
@@ -82,41 +88,8 @@ class JuliaGraphView : View {
      * I would actually benchmark properly and measure
      */
     fun updateParamsAndRedraw(stepSizePx: Int, graphRules: GraphRules) {
-
-        presentFuture?.cancel(false)
-
-        presentFuture = precomputeExector.submit {
-            try {
-                val canvas = Canvas(bitmap)
-                canvas.drawRect(0f, 0f, bitmap!!.width.toFloat(), bitmap!!.height.toFloat(), divergentPaint) // Clear canvas
-
-                for (x in 0..bitmap!!.width step stepSizePx) {
-                    val r = graphRules.minReal + (graphRules.maxReal - graphRules.minReal) * x / bitmap!!.width
-                    for (y in 0..bitmap!!.height step stepSizePx) {
-                        val i = graphRules.minImaginary + (graphRules.maxImaginary - graphRules.minImaginary) * y / bitmap!!.height
-                        if (isIn(r, i, graphRules)) {
-                            canvas.drawRect(x.toFloat(), // Chunk into squares of size stepSize to reduce the amount of computation required
-                                    y.toFloat(),
-                                    (x + stepSizePx).toFloat(),
-                                    (y + stepSizePx).toFloat(),
-                                    convergentPaint)
-                        }
-                    }
-                }
-
-                mainThreadHandler.post {
-                    presentFuture = null
-                    this.invalidate()
-                }
-            } catch (interruptedException: InterruptedException) {
-                println("Was cancelled")
-                // Too late, a new render has been scheduled, drop render
-            }
-        }
-    }
-
-    override fun onDetachedFromWindow() {
-        precomputeExector.shutdown()
-        super.onDetachedFromWindow()
+        this.stepSizePx = stepSizePx
+        this.graphRules = graphRules
+        this.invalidate()
     }
 }
